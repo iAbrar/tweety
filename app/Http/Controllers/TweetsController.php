922fe4c21c8e62d1c6b44cd1e94feddd5a4843c4
Abrar <abrar.ksu@gmail.com>
2021-07-11 21:38:42 +0300

    feat: create tweet

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tweet;
 
 class TweetsController extends Controller
 {
-    //
+    public function store(){
+
+      $attributes = request()->validate(['body' => 'required|max:255']);
+
+      Tweet::create([
+        'user_id' => auth()->user()->id,
+        'body' => $attributes['body']
+      ]);
+
+      return redirect('/home');
+
+    }
 }
