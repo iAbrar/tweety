9db6181f368b12f75944ff39ed11c3c51777fe21
Abrar <abrar.ksu@gmail.com>
2021-07-11 21:38:15 +0300

    feat: add tweet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tweet;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      $tweets = Tweet::latest()->get();
+        return view('home',[
+          'tweets' => $tweets
+        ]);
     }
 }
--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,32 +1,34 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form method="POST" action="/tweets">
-        @csrf
+  <form method="POST" action="/tweets">
+    @csrf
 
-        <textarea
-            name="body"
-            class="w-full"
-            placeholder="What's up doc?"
-        ></textarea>
+    <textarea
+    name="body"
+    class="w-full"
+    placeholder="What's up doc?"
+    
 
-        <hr class="my-4">
+    ></textarea>
 
-        <footer class="flex justify-between">
-            <img
-                src="{{ auth()->user()->avatar }}"
-                alt="your avatar"
-                class="rounded-full mr-2"
-            >
+    <hr class="my-4">
 
-            <button
-                type="submit"
-                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
-            >
-                Tweet-a-roo!
-            </button>
-        </footer>
-    </form>
+    <footer class="flex justify-between">
+      <img
+      src="{{ auth()->user()->avatar }}"
+      alt="your avatar"
+      class="rounded-full mr-2"
+      >
 
-    @error('body')
-        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-    @enderror
+      <button
+      type="submit"
+      class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
+      >
+      Tweet-a-roo!
+    </button>
+  </footer>
+</form>
+
+@error('body')
+  <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+@enderror
 </div>
